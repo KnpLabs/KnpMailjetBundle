4e045d46ed3e81812da88c5fbfe7a3dd8b4a244a
Roman Marintšenko <inoryy@gmail.com>
2013-08-09 16:20:58 +0300

    implement helper methods for endpoint action

--- Controller/EventController.php
+++ Controller/EventController.php
@@ -3,6 +3,7 @@
 namespace Knp\Bundle\MailjetBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 use Mailjet\Event\EventFactoryInterface;
 
@@ -17,6 +18,43 @@ class EventController
 
     public function handleEventAction(Request $request)
     {
-        
+        if ($this->validateRequest($request)) {
+            $status = 200;
+
+            $this->processRequest($request);
+        } else {
+            $status = 404;
+        }
+
+        return $this->prepareResponse($status);
+    }
+
+    protected function processRequest(Request $request)
+    {
+        $data = $this->extractData($request);
+        $event = $this->eventFactory->createEvent($data);
+    }
+
+    private function extractData(Request $request)
+    {
+        return json_decode($request->getContent(), true);
+    }
+
+    private function prepareResponse($status)
+    {
+        return new JsonResponse(array(), $status);
+    }
+
+    private function validateRequest(Request $request)
+    {
+        if (!$request->isMethod('POST')) {
+            return false;
+        }
+
+        if (!'application/json' !== $request->getContentType()) {
+            return false;
+        }
+
+        return true;
     }
 }
