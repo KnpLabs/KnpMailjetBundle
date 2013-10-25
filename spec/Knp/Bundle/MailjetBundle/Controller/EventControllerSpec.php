89259ee52a287a3594682ebe511e3d024c3bb4b7
Roman Marintšenko <inoryy@gmail.com>
2013-10-25 11:08:34 +0300

    add successful callback spec

--- spec/Knp/Bundle/MailjetBundle/Controller/EventControllerSpec.php
+++ spec/Knp/Bundle/MailjetBundle/Controller/EventControllerSpec.php
@@ -5,12 +5,13 @@ namespace spec\Knp\Bundle\MailjetBundle\Controller;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
-use Mailjet\Event\EventFactoryInterface;
-use Symfony\Component\EventDispatcher\EventDispatcherInterface;
-
 class EventControllerSpec extends ObjectBehavior
 {
-    function let(EventFactoryInterface $eventFactory, EventDispatcherInterface $eventDispatcher)
+    /**
+     * @param \Mailjet\Event\EventFactoryInterface                        $eventFactory
+     * @param \Symfony\Component\EventDispatcher\EventDispatcherInterface $eventDispatcher
+     */
+    function let($eventFactory, $eventDispatcher)
     {
         $this->beConstructedWith($eventFactory, $eventDispatcher);
     }
@@ -18,5 +19,29 @@ class EventControllerSpec extends ObjectBehavior
     function it_is_initializable()
     {
         $this->shouldHaveType('Knp\Bundle\MailjetBundle\Controller\EventController');
+    }
+
+    /**
+     * @param \Mailjet\Event\EventFactoryInterface                        $eventFactory
+     * @param \Symfony\Component\EventDispatcher\EventDispatcherInterface $eventDispatcher
+     * @param \Symfony\Component\HttpFoundation\Request                   $request
+     * @param \Knp\Bundle\MailjetBundle\Event\Adapter\OpenEvent           $event
+     */
+    function it_dispatches_event_on_successful_callback($eventFactory, $eventDispatcher, $request, $event)
+    {
+        $data = array('event' => 'open');
+        $type = 'knp_mailjet.open';
+
+        $request->isMethod('POST')->shouldBeCalled()->willReturn(true);
+        $request->getContent()->shouldBeCalled()->willReturn(json_encode($data));
+
+        $event->getType()->shouldBeCalled()->willReturn($type);
+
+        $eventFactory->createEvent($data)->shouldBeCalled()->willReturn($event);
+        $eventDispatcher->dispatch($type, $event)->shouldBeCalled();
+
+        $response = $this->handleEventAction($request);
+        $response->shouldHaveType('Symfony\Component\HttpFoundation\JsonResponse');
+        $response->getStatusCode()->shouldReturn(200);
     }
 }
