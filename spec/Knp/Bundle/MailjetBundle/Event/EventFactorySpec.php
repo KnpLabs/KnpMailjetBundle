8ca477002b4ca2e9afc4e0a2e38b6cacebd2bb14
Roman Marintšenko <inoryy@gmail.com>
2013-10-25 10:54:41 +0300

    add event factory spec

--- spec/Knp/Bundle/MailjetBundle/Event/EventFactorySpec.php
+++ spec/Knp/Bundle/MailjetBundle/Event/EventFactorySpec.php
@@ -11,4 +11,13 @@ class EventFactorySpec extends ObjectBehavior
     {
         $this->shouldHaveType('Knp\Bundle\MailjetBundle\Event\EventFactory');
     }
+
+    function it_creates_event_adapters()
+    {
+        $eventAdapter = $this->createEvent(array('event' => 'open'));
+        $eventAdapter->shouldHaveType('Knp\Bundle\MailjetBundle\Event\Adapter\OpenEvent');
+        $eventAdapter->getType()->shouldReturn('knp_mailjet.open');
+        $event = $eventAdapter->getEvent();
+        $event->shouldHaveType('Mailjet\Event\Events\OpenEvent');
+    }
 }
