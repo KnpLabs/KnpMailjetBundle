901f9ccd7b06a604376828d73407c2ebb494ed31
Roman Marintšenko <inoryy@gmail.com>
2013-10-25 11:15:03 +0300

    add controller callback validation specs

--- spec/Knp/Bundle/MailjetBundle/Controller/EventControllerSpec.php
+++ spec/Knp/Bundle/MailjetBundle/Controller/EventControllerSpec.php
@@ -7,13 +7,15 @@ use Prophecy\Argument;
 
 class EventControllerSpec extends ObjectBehavior
 {
+    private $token = 12345;
+
     /**
      * @param \Mailjet\Event\EventFactoryInterface                        $eventFactory
      * @param \Symfony\Component\EventDispatcher\EventDispatcherInterface $eventDispatcher
      */
     function let($eventFactory, $eventDispatcher)
     {
-        $this->beConstructedWith($eventFactory, $eventDispatcher);
+        $this->beConstructedWith($eventFactory, $eventDispatcher, $this->token);
     }
 
     function it_is_initializable()
@@ -27,7 +29,7 @@ class EventControllerSpec extends ObjectBehavior
      * @param \Symfony\Component\HttpFoundation\Request                   $request
      * @param \Knp\Bundle\MailjetBundle\Event\Adapter\OpenEvent           $event
      */
-    function it_dispatches_event_on_successful_callback($eventFactory, $eventDispatcher, $request, $event)
+    function it_dispatches_event_on_successful_callback_and_proper_token($eventFactory, $eventDispatcher, $request, $event)
     {
         $data = array('event' => 'open');
         $type = 'knp_mailjet.open';
@@ -40,8 +42,29 @@ class EventControllerSpec extends ObjectBehavior
         $eventFactory->createEvent($data)->shouldBeCalled()->willReturn($event);
         $eventDispatcher->dispatch($type, $event)->shouldBeCalled();
 
-        $response = $this->handleEventAction($request);
+        $response = $this->handleEventAction($request, $this->token);
         $response->shouldHaveType('Symfony\Component\HttpFoundation\JsonResponse');
         $response->getStatusCode()->shouldReturn(200);
+    }
+
+    /**
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     */
+    function it_does_not_dispatch_event_on_invalid_callback($request)
+    {
+        $request->isMethod('POST')->shouldBeCalled()->willReturn(false);
+        $response = $this->handleEventAction($request, $this->token);
+        $response->shouldHaveType('Symfony\Component\HttpFoundation\JsonResponse');
+        $response->getStatusCode()->shouldReturn(404);
+    }
+
+    /**
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     */
+    function it_does_not_dispatch_event_on_invalid_token($request)
+    {
+        $response = $this->handleEventAction($request, 54321);
+        $response->shouldHaveType('Symfony\Component\HttpFoundation\JsonResponse');
+        $response->getStatusCode()->shouldReturn(404);
     }
 }
